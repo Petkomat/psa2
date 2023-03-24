5d7eea83a61f8e0df13daa43d1d5404b1de528f9
Matej Petkovic <matej.petkovic@ijs.si>
2023-03-24 15:20:19 +0100

    velikost unije

--- leto2223_koda/vaje07/README.md
+++ leto2223_koda/vaje07/README.md
@@ -31,14 +31,19 @@ Pri odgovarjanju na poizvedbe moramo trenutne vsote posodabljati s pomočjo vred
 
 ## Dolžina unije
 
+
 Kar se posodobitev tiče, dovoljujemo
 
 - dodajanje poljubnega intervala (tj. `dodaj(i, j, x)` za `x > 0`),
 - odstranjevanje obstoječega intervala (tj. `dodaj(i, j, -x)` za `x > 0`, za katerega smo pred tem izvedli `dodaj(i, j, x)`).
 
-TODO :)
+Če poskusimo odstraniti nekaj, česar še nismo dodali, koda ne bo delovala.
 
-## Hakeljci
+- $\alpha$: vsi listi v danem poddrevesu ležijo v toliko intervalih,
+- $\beta$: trenutno število pozitivnih elementov seznama, ki pripadajo listom poddrevesa (ob upoštevanju le tistih popravkov, ki so bili omejeni na to poddrevo).
+
+
+## Implementacija
 
 Implementacijo si zelo olajšamo, če naredimo polno drevo, saj ima to nekaj prijetnih posledic:
 
@@ -50,14 +55,31 @@ Implementacijo si zelo olajšamo, če naredimo polno drevo, saj ima to nekaj pri
 - list, ki pripada indeksu $i$, najdemo v konstantnem času (ker vemo, koliko notranjih vozlišč je treba preskočiti)
 - ...
 
-Vseeno je vse skupaj kar tečno za implementacijo. Psevdokoda za odgovore na poizvedbe je približno taka:
+Vseeno je vse skupaj kar tečno za implementacijo.
 
-1. Naredi polno drevo, ki ima vsaj $n$ listov. Prvih $n$ listov napolni z vrednostmi $a_i$, preostale pa z nevtralnimi vrednostmi (srednje lahko).
-2. Najdi lista v drevesu, ki pripada indeksoma $i$ in $j$ (lahko), in njunega prvega skupnega prednika $p$. (lahko)
-3. Najdi (levo) pot od lista za $i$ do $p$ in (desno) pot od lista za $j$ do $p$. (lahko)
-4. Postopoma računaj ločena odgovora vzdolž leve in desne poti od spodaj navzgor. (zelo tečno)
-5. Združi odgovora v skupnega in ga posodobi s pomočjo vrednosti $\alpha$ na poti od $p$ do korena. (tečno)
+Najprej naredimo polno drevo, ki ima vsaj $n$ listov. Prvih $n$ listov napolni z vrednostmi $a_i$, preostale pa z nevtralnimi vrednostmi (srednje lahko).
 
-Pri 4. in 5. je treba zelo paziti, da česa ne posodobimo dvakrat (npr. če je $i = j$ ali sta $i$ in $j$ otroka od $p$ ali ...) itd. itd.
+### Poizvedbe
+
+1. Najdi lista v drevesu, ki pripada indeksoma $i$ in $j$ (lahko), in njunega prvega skupnega prednika $p$. (lahko)
+2. Najdi (levo) pot od lista za $i$ do $p$ in (desno) pot od lista za $j$ do $p$. (lahko)
+3. Postopoma računaj ločena odgovora vzdolž leve in desne poti od spodaj navzgor. (zelo tečno)
+4. Združi odgovora v skupnega in ga posodobi s pomočjo vrednosti $\alpha$ na poti od $p$ do korena. (tečno)
+
+Pri 3. in 4. je treba zelo paziti, da česa ne posodobimo dvakrat (npr. če je $i = j$ ali sta $i$ in $j$ otroka od $p$ ali ...) itd. itd.
+
+### Posodobitve
+
+1. Najdi lista v drevesu, ki pripada indeksoma $i$ in $j$ (lahko), in njunega prvega skupnega prednika $p$. (lahko)
+2. Najdi (levo) pot od lista za $i$ do $p$ in (desno) pot od lista za $j$ do $p$. (lahko)
+3. Najdi korene dreves, ki jih objemata leva in desna pot (njihovi listi skupaj z listom za $i$ in listom za $j$ natanko pokrijejo interval, ki ga posodabljamo). (kar lahko)
+4. Posodobi vrednosti $\alpha$ v obeh listih ter najdenih korenih. (malce tečno)
+5. Posodobi vrednosti $\beta$ v najdenih korenih in vzdolž obeh poti (od spodaj navzgor) (malce tečno).
+
+
+
+
+
+
 
 
--- leto2223_koda/vaje07/drevesa.py
+++ leto2223_koda/vaje07/drevesa.py
@@ -1,4 +1,6 @@
 from typing import List, Tuple
+import random
+import tqdm
 
 
 class StatAlfaBeta:
@@ -11,10 +13,7 @@ class StatAlfaBeta:
         for i, x in enumerate(values):
             self.nodes[i + offset][0] = x
         for i_this in range(len(self.nodes) - 1, -1, -1):
-            if self.is_internal(i_this):
-                self.update_beta_from_children(i_this)
-            else:
-                self.nodes[i_this][1] = self.nodes[i_this][0]
+            self.update_beta(i_this)
 
     def __str__(self):
         lines = [[]]
@@ -22,7 +21,7 @@ class StatAlfaBeta:
         processed_line = 0
         width = 1
         for alpha, beta in self.nodes:
-            lines[-1].append(f"({alpha: 2}, {beta: 2})")
+            lines[-1].append(f"({alpha: 3}, {beta: 3})")
             processed_line += 1
             if n_this_line == processed_line:
                 processed_line = 0
@@ -33,18 +32,51 @@ class StatAlfaBeta:
         lines.pop()
         # cca. curr_length + presl * L <= width
         for i in range(len(lines)):
-            joiner = " " * ((width - sum(map(len, lines[i]))) // len(lines[i]))
-            lines[i] = joiner.join(lines[i]).center(width, " ")
+            line_length = sum(map(len, lines[i]))
+            n_spaces = len(lines[i])
+            if i == len(lines) - 1:
+                n_spaces -= 1
+            space = " " * ((width - line_length) // n_spaces)
+            lines[i] = space.join(lines[i]).center(width, " ")
         return "\n".join(lines)
 
-    def update_beta_from_children(self, i: int):
+    def update_beta(self, i: int):
         raise NotImplementedError()
 
     def get_value(self, i: int, j: int):
         raise NotImplementedError()
 
+    def get_paths_and_stuff(self, i: int, j: int):
+        if not (0 <= i <= j < self.n_nodes):
+            raise ValueError(f"Not true: 0 <= {i} <= {j} <= {self.n_nodes}")
+        i_left = self.find_leaf(i)
+        i_right = self.find_leaf(j)
+        path_left = StatAlfaBeta.get_path_from_leaf(i_left)
+        path_right = StatAlfaBeta.get_path_from_leaf(i_right)
+        i_common, below_common = StatAlfaBeta.compute_common_and_paths_below(path_left, path_right)
+        return i_left, i_right, path_left, path_right, i_common, below_common
+
     def increment(self, i: int, j: int, x: int):
-        raise NotImplementedError()
+        i_left, i_right, path_left, path_right, i_common, below_common = self.get_paths_and_stuff(i, j)
+        internal_nodes = StatAlfaBeta.compute_internal_roots(*below_common)
+        # increase alphas
+        if i_left == i_right:
+            self.nodes[i_left][0] += x
+        else:
+            for node in [i_left, i_right]:
+                self.nodes[node][0] += x
+        for root in internal_nodes:
+            self.nodes[root][0] += x
+        # adjust betas
+        for root in internal_nodes:
+            self.update_beta(root)
+        found_common = False
+        for left, right in zip(path_left, path_right):
+            if left == i_common:
+                found_common = True
+            self.update_beta(left)
+            if not found_common:
+                self.update_beta(right)
 
     def find_leaf(self, i: int):
         return i + self.offset
@@ -67,6 +99,22 @@ class StatAlfaBeta:
 
     def is_internal(self, i: int):
         return 2 * i + 1 < len(self.nodes)
+
+    @staticmethod
+    def should_update_with_internal(i, path, j, node):
+        root = -1
+        update_with_internal = False
+        if j == 0:
+            root = node
+        elif i == 0 and StatAlfaBeta.get_right(node) != path[j - 1]:
+            # internal on left path
+            root = StatAlfaBeta.get_right(node)
+            update_with_internal = True
+        elif i == 1 and StatAlfaBeta.get_left(node) != path[j - 1]:
+            # internal on right
+            root = StatAlfaBeta.get_left(node)
+            update_with_internal = True
+        return root, update_with_internal
 
     @staticmethod
     def get_path_from_leaf(i_leaf: int):
@@ -93,6 +141,18 @@ class StatAlfaBeta:
             paths_bellow_common[0].extend(path_left[1:i_common])
             paths_bellow_common[1].extend(path_right[1:i_common])
         return path_left[i_common], paths_bellow_common
+
+    @staticmethod
+    def compute_internal_roots(path_left: List[int], path_right: List[int]) -> List[int]:
+        internal = []
+        for i in range(1, len(path_left)):
+            left = path_left[i]
+            right = path_right[i]
+            if StatAlfaBeta.get_left(left) == path_left[i - 1]:
+                internal.append(StatAlfaBeta.get_right(left))
+            if StatAlfaBeta.get_right(right) == path_right[i - 1]:
+                internal.append(StatAlfaBeta.get_left(right))
+        return internal
 
     @staticmethod
     def find_full_tree_size(n: int) -> Tuple[int, int]:
@@ -122,21 +182,14 @@ class SumOfRange(StatAlfaBeta):
     def __init__(self, values: List[int]):
         super().__init__(values, 0, 0)
 
-    def update_beta_from_children(self, i: int):
-        l_beta = self.nodes[StatAlfaBeta.get_left(i)][1]
-        r_beta = self.nodes[StatAlfaBeta.get_right(i)][1]
-        alfa = self.nodes[i][0]
-        self.nodes[i][1] = l_beta + r_beta + self.n_leaves_in_subtree(i) * alfa
-
-    def get_paths_and_stuff(self, i: int, j: int):
-        if not (0 <= i <= j < self.n_nodes):
-            raise ValueError(f"Not true: 0 <= {i} <= {j} <= {self.n_nodes}")
-        i_left = self.find_leaf(i)
-        i_right = self.find_leaf(j)
-        path_left = StatAlfaBeta.get_path_from_leaf(i_left)
-        path_right = StatAlfaBeta.get_path_from_leaf(i_right)
-        i_common, below_common = StatAlfaBeta.compute_common_and_paths_below(path_left, path_right)
-        return i_left, i_right, path_left, path_right, i_common, below_common
+    def update_beta(self, i: int):
+        if self.is_internal(i):
+            l_beta = self.nodes[StatAlfaBeta.get_left(i)][1]
+            r_beta = self.nodes[StatAlfaBeta.get_right(i)][1]
+            alfa = self.nodes[i][0]
+            self.nodes[i][1] = l_beta + r_beta + self.n_leaves_in_subtree(i) * alfa
+        else:
+            self.nodes[i][1] = self.nodes[i][0]
 
     def get_value(self, i: int, j: int):
         i_left, i_right, path_left, path_right, i_common, below_common = self.get_paths_and_stuff(i, j)
@@ -160,69 +213,12 @@ class SumOfRange(StatAlfaBeta):
 
     def get_value_along_path(self, i, path, value_left_right, n_nodes_left_right):
         for j, node in enumerate(path):
-            update_with_internal = False
-            root = -1
-            if j == 0:
-                root = node
-            elif i == 0 and StatAlfaBeta.get_right(node) != path[j - 1]:
-                # internal on left path
-                root = StatAlfaBeta.get_right(node)
-                update_with_internal = True
-            elif i == 1 and StatAlfaBeta.get_left(node) != path[j - 1]:
-                # internal on right
-                root = StatAlfaBeta.get_left(node)
-                update_with_internal = True
+            root, update_with_internal = StatAlfaBeta.should_update_with_internal(i, path, j, node)
             if update_with_internal:
                 value_left_right[i] += self.nodes[root][1]
             if root >= 0:
                 n_nodes_left_right[i] += self.n_leaves_in_subtree(root)
             value_left_right[i] += self.nodes[node][0] * n_nodes_left_right[i]
-
-    def increment(self, i: int, j: int, x: int):
-        i_left, i_right, path_left, path_right, i_common, below_common = self.get_paths_and_stuff(i, j)
-        if i_left == i_right:
-            self.nodes[i_left][0] += x  # increase only alpha
-            increment = x
-        elif StatAlfaBeta.get_parent(i_left) == StatAlfaBeta.get_parent(i_right):
-            for node in [i_left, i_right]:
-                for i in range(2):
-                    self.nodes[node][i] += x
-            increment = 2 * x
-        else:
-            increment_left_right = [0, 0]
-            for i, path in enumerate(below_common):
-                self.increment_along_path(i, path, increment_left_right, x)
-            increment = sum(increment_left_right)
-        while i_common >= 0:
-            self.nodes[i_common][1] += increment
-            i_common = StatAlfaBeta.get_parent(i_common)
-
-    def increment_along_path(self, i, path, increment_left_right, x):
-        for j, node in enumerate(path):
-            increase_parent = True
-            if j == 0:
-                root = node
-                increase_parent = False
-            elif i == 0 and StatAlfaBeta.get_right(node) != path[j - 1]:
-                # internal on left path
-                root = StatAlfaBeta.get_right(node)
-            elif i == 1 and StatAlfaBeta.get_left(node) != path[j - 1]:
-                # ... on right
-                root = StatAlfaBeta.get_left(node)
-            else:
-                root = -1
-            # pass the total increment so far
-            self.nodes[node][1] += increment_left_right[i]
-            if root <= 0:
-                continue
-            # update the internal root ...
-            n_leaves = self.n_leaves_in_subtree(root)
-            self.nodes[root][0] += x
-            self.nodes[root][1] += n_leaves * x
-            if increase_parent:
-                # ... and parent if this is not i_left or i_right
-                self.nodes[node][1] += n_leaves * x
-            increment_left_right[i] += n_leaves * x
 
 
 class SumOfRangeBrute:
@@ -241,36 +237,119 @@ class SumOfRangeBrute:
 
 
 class UnionSize(StatAlfaBeta):
-    def update_beta_from_children(self, i: int):
-        i_left = 2 * i + 1
-        i_right = 2 * i + 2
-        if self.nodes[i][0] > 0:
+    def update_beta(self, i: int):
+        if not self.is_internal(i):
+            self.nodes[i][1] = self.nodes[i][0]
+        elif self.nodes[i][0] > 0:
             self.nodes[i][1] = self.n_leaves_in_subtree(i)
         else:
-            self.nodes[i][1] = self.nodes[i_left][1] + self.nodes[i_right][1]
+            l_beta = self.nodes[StatAlfaBeta.get_left(i)][1]
+            r_beta = self.nodes[StatAlfaBeta.get_right(i)][1]
+            self.nodes[i][1] = l_beta + r_beta
 
     def get_value(self, i: int, j: int):
-        pass
+        """
+        Until the while loop, it is the same as in the SumOfRange case.
+        :param i:
+        :param j:
+        :return:
+        """
+        i_left, i_right, path_left, path_right, i_common, below_common = self.get_paths_and_stuff(i, j)
+        if i_left == i_right:
+            final_value = 0
+            n_nodes = 1
+        elif StatAlfaBeta.get_parent(i_left) == StatAlfaBeta.get_parent(i_right):
+            final_value = self.nodes[i_left][1] + self.nodes[i_right][1]
+            n_nodes = 2
+        else:
+            n_nodes_left_right = [0, 0]
+            value_left_right = [0, 0]
+            for i, path in enumerate(below_common):
+                self.get_value_along_path(i, path, value_left_right, n_nodes_left_right)
+            final_value = sum(value_left_right)
+            n_nodes = sum(n_nodes_left_right)
+        while i_common >= 0:
+            if self.nodes[i_common][0] > 0:
+                return n_nodes  # won't change any more
+            i_common = StatAlfaBeta.get_parent(i_common)
+        return final_value
 
-    def increment(self, i: int, j: int, x: int):
-        pass
+    def get_value_along_path(self, i, path, value_left_right, n_nodes_left_right):
+        for j, node in enumerate(path):
+            root, update_with_internal = StatAlfaBeta.should_update_with_internal(i, path, j, node)
+            internal_value = self.nodes[root][1] if update_with_internal else 0
+            if root >= 0:
+                n_nodes_left_right[i] += self.n_leaves_in_subtree(root)
+            if self.nodes[node][0] > 0:
+                value_left_right[i] = n_nodes_left_right[i]
+            else:
+                value_left_right[i] += internal_value
 
     def __init__(self, values: List[int]):
         super().__init__(values, 0, 0)
 
 
+class UnionSizeBrute:
+    def __init__(self, values: List[int]):
+        self.values = values[:]
+
+    def __str__(self):
+        return " ".join(map(str, self.values))
+
+    def increment(self, i, j, x):
+        for k in range(i, j + 1):
+            self.values[k] += x
+            if self.values[k] < 0:
+                raise ValueError(f"{self.values}")
+
+    def get_value(self, i, j):
+        return sum(x > 0 for x in self.values[i: j + 1])
+
+
 def test_sum_range():
-    n = 100
-    lst = list(range(n))
-    t = SumOfRange(lst)
-    b = SumOfRangeBrute(lst)
+    for n in range(13, 68):
+        lst = list(range(n))
+        t = SumOfRange(lst)
+        b = SumOfRangeBrute(lst)
+        for i in range(n):
+            for j in range(i, n):
+                t.increment(i, j, (i + 1) * (j + 1))
+                b.increment(i, j, (i + 1) * (j + 1))
+                if b.get_value(i, j) != t.get_value(i, j):
+                    raise ValueError(f"{n}: [{i}, {j}]: {b.get_value(i, j)} != {t.get_value(i, j)}")
+
+
+def test_union_size():
+    """
+    Vsak interval 3x doda, nato pa (v drugem vrstnem redu) 3x odstrani.
+    Po vsakem dodajanju in odstranjevanju preveri.
+    :return:
+    """
+    random.seed(1234)
+    for n in range(27, 34):
+        lst = list(range(n))
+        t = UnionSize(lst)
+        b = UnionSize(lst)
+        intervals = [(i, j) for _ in range(3) for i in range(n) for j in range(i, n)]
+        random.shuffle(intervals)
+        for start, end in tqdm.tqdm(intervals):
+            t.increment(start, end, 1)
+            b.increment(start, end, 1)
+            test_all_us(t, b, n)
+        random.shuffle(intervals)
+        for start, end in tqdm.tqdm(intervals):
+            t.increment(start, end, -1)
+            b.increment(start, end, -1)
+            test_all_us(t, b, n)
+
+
+def test_all_us(tree, brute, n):
     for i in range(n):
         for j in range(i, n):
-            t.increment(i, j, (i + 1) * (j + 1))
-            b.increment(i, j, (i + 1) * (j + 1))
-            if b.get_value(i, j) != t.get_value(i, j):
-                raise ValueError(f"{n}: [{i}, {j}]: {b.get_value(i, j)} != {t.get_value(i, j)}")
+            if brute.get_value(i, j) != tree.get_value(i, j):
+                raise ValueError(f"{n}: [{i}, {j}]: {brute.get_value(i, j)} != {tree.get_value(i, j)}")
 
 
 if __name__ == '__main__':
     test_sum_range()
+    test_union_size()
